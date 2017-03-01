0f9660103b5ab1690b467b05b4b67605ab7bb367
Glendon Cheney <glendon.cheney@gmail.com>
2017-02-28 19:42:34 -0600

    Create contact management component

---
+++ src/components/contact/ContactPage.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+class ContactPage extends React.Component {
+    render() {
+        return (
+            <div className="well">
+                <h1 className="text-center">Your Contacts</h1>
+            </div>
+        );
+    }
+}
+
+export default ContactPage;
